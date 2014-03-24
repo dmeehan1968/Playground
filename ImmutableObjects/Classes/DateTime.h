eb033212bc42665f107f2f7c27f5379a513b9e45
Dave Meehan <dave_meehan@replicated.co.uk>
2014-03-24 14:21:28 +0100

    [ImmutableObjects] Added DateTime constructor

--- ImmutableObjects/Classes/DateTime.h
+++ ImmutableObjects/Classes/DateTime.h
@@ -9,7 +9,30 @@
 #ifndef __ImmutableObjects__DateTime__
 #define __ImmutableObjects__DateTime__
 
+#include <ctime>
+
 class DateTime {
+    
+public:
+    
+    DateTime(const unsigned year, const unsigned month, const unsigned day, const unsigned hour = 0, const unsigned minute = 0, const unsigned second = 0) {
+        
+        struct tm tm;
+        
+        tm.tm_year = year;
+        tm.tm_mon = month;
+        tm.tm_mday = day;
+        tm.tm_hour = hour;
+        tm.tm_min = minute;
+        tm.tm_sec = second;
+        
+        _time = mktime(&tm);
+        
+    }
+    
+private:
+    
+    time_t _time;
     
 };
 

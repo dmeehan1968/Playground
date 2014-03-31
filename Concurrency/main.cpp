6e057949aa7a86733172f37d61995abdf5c590f9
Dave Meehan <dave_meehan@replicated.co.uk>
2014-03-31 20:00:55 +0200

    Added Concurrency project stub

---
+++ Concurrency/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Concurrency
+//
+//  Created by Dave Meehan on 31/03/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+    
+}

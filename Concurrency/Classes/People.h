a56fb37dec70b77deafa5ff7d1450e1b3d185bb2
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-02 19:21:28 +0200

    Refactored stream output into Decorator class

--- Concurrency/Classes/People.h
+++ Concurrency/Classes/People.h
@@ -15,32 +15,81 @@
 #include <iostream>
 #include <future>
 
-class People : public std::vector<Person> {
+class PeopleNewlineDecorator {
+    
+public:
+    
+    PeopleNewlineDecorator(std::ostream &stream) : _stream(stream) {}
+    
+    void before() const {}
+    
+    void between() const {
+        _stream << ", " << std::endl;
+    }
+    
+    void after() const {
+        _stream << std::endl;
+    }
+    
+    template <class Iterator>
+    void decorate(const Iterator &begin, const Iterator &end) const {
+        
+        int count = 0;
+        
+        if (end > begin) {
+            before();
+        }
+        
+        std::for_each(begin, end, [&](const Person &person) {
+            
+            if (count++ > 0) {
+                between();
+            }
+            
+            decorate(person);
+            
+        });
+        
+        if (count) {
+            after();
+        }
+
+    }
+    
+    void decorate(const Person &person) const {
+        _stream << person;
+    }
+    
+private:
+    
+    std::ostream &_stream;
+    
+};
+
+class People {
     
 public:
     
     People(const std::initializer_list<Person> &people)
     :
-        std::vector<Person>(people)
+        _people(people)
     {}
 
+    void decorate(const PeopleNewlineDecorator &decorator) const {
+        
+        decorator.decorate(_people.begin(), _people.end());
+
+    }
+    
+private:
+    
+    std::vector<Person> _people;
+    
 };
 
 inline std::ostream &operator << (std::ostream &stream, const People &people) {
     
-    bool delim = false;
-    
-    std::for_each(people.begin(), people.end(), [&stream, &delim](const Person &person) {
-        if (delim) {
-            stream << ", " << std::endl;
-        }
-        delim = true;
-        stream << person;
-    });
-    
-    if (people.size() > 0) {
-        stream << std::endl;
-    }
+    people.decorate(PeopleNewlineDecorator(stream));
     
     return stream;
     

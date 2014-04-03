cd8a5b6721173baf97051ad6eef1a4459e83a17f
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-03 20:48:49 +0200

    [ConcurrentContacts] Added project

---
+++ ConcurrentContacts/ConcurrentContacts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ConcurrentContacts
+//
+//  Created by Dave Meehan on 03/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+}
+

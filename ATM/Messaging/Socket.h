fb3ff30c853aac844dc0c9ad662b2310a198c3e8
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-16 19:21:42 +0100

    [ATM] Extracted Dispatcher to own module

--- ATM/Messaging/Socket.h
+++ ATM/Messaging/Socket.h
@@ -13,6 +13,7 @@
 #include "Exception.h"
 #include "ISerialisable.h"
 #include "MessageIterator.h"
+#include "Dispatcher.h"
 
 #include <string>
 #include <functional>
@@ -83,48 +84,6 @@ namespace Messaging {
             return len;
             
         }
-        
-        template <class T>
-        class Dispatcher {
-            
-        public:
-            Dispatcher(const std::shared_ptr<T> msg)
-            :
-                _msg(msg),
-                _handled(false)
-            {}
-            
-            template <class M>
-            Dispatcher &handle(const std::function<bool(const M&)> &func) {
-                
-                if ( ! _handled) {
-                    
-                    auto specific = std::dynamic_pointer_cast<M>(_msg);
-                    
-                    if (specific) {
-                        _handled = func(*specific);
-                    }
-                    
-                }
-                
-                return *this;
-            }
-            
-            Dispatcher &handle(const std::function<bool()> &func) {
-                
-                if (!_handled) {
-                    _handled = func();
-                }
-                return *this;
-                
-            }
-            
-        private:
-            
-            std::shared_ptr<T> _msg;
-            bool _handled;
-            
-        };
         
         class NilFactory {
             

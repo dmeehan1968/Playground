ee1648560c6d167af4fe5c34746fac582bdfddbe
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-17 12:59:19 +0100

    [ZMQ_STREAM] Added project base

---
+++ ZMQ_STREAM/ZMQ_STREAM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ZMQ_STREAM
+//
+//  Created by Dave Meehan on 17/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

243705813bfa35d4083c1d7586de61e9105309da
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-21 15:01:07 +0100

    [Messaging] Added Message class and spec stub

---
+++ Messaging/Classes/Message.h
@@ -0,0 +1,22 @@
+//
+//  Message.h
+//  Messaging
+//
+//  Created by Dave Meehan on 21/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#ifndef __Messaging__Message__
+#define __Messaging__Message__
+
+#include "Socket.h"
+
+namespace Messaging {
+    
+    class Message {
+        
+    };
+    
+}
+
+#endif /* defined(__Messaging__Message__) */
---
+++ Messaging/Tests/MessageSpec.cpp
@@ -0,0 +1,21 @@
+//
+//  MessageSpec.cpp
+//  Messaging
+//
+//  Created by Dave Meehan on 21/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <ZingBDD/ZingBDD.h>
+
+#include "Message.h"
+
+namespace Messaging { namespace Specs {
+  
+    describe(Message, {
+    
+        Message msg;
+        
+    });
+    
+} }

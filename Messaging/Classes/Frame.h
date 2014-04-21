5c994d7688a6e608e884d2885e0915bb936029b2
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-21 12:48:24 +0100

    [Messaging] Added Frame class and spec stubs

---
+++ Messaging/Classes/Frame.h
@@ -0,0 +1,22 @@
+//
+//  Frame.h
+//  Messaging
+//
+//  Created by Dave Meehan on 21/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#ifndef __Messaging__Frame__
+#define __Messaging__Frame__
+
+#include "Socket.h"
+
+namespace Messaging {
+
+    class Frame {
+        
+    };
+
+}
+
+#endif /* defined(__Messaging__Frame__) */
---
+++ Messaging/Tests/FrameSpec.cpp
@@ -0,0 +1,21 @@
+//
+//  FrameSpec.cpp
+//  Messaging
+//
+//  Created by Dave Meehan on 21/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <ZingBDD/ZingBDD.h>
+
+#include "Frame.h"
+
+namespace Messaging { namespace Specs {
+   
+    describe(Frame, {
+    
+        Frame frame;
+        
+    });
+    
+} }

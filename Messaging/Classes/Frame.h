e5455629ed11396bd6f721206a954287448ef996
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-21 13:13:07 +0100

    [Messaging] Added Frame constructors, default, sized and from data

--- Messaging/Classes/Frame.h
+++ Messaging/Classes/Frame.h
@@ -10,10 +10,51 @@
 #define __Messaging__Frame__
 
 #include "Socket.h"
+#include <cstdlib>
 
 namespace Messaging {
 
     class Frame {
+        
+    public:
+        
+        Frame() {
+            zmq_msg_init(&_msg);
+        }
+        
+        Frame(const size_t size) {
+            zmq_msg_init_size(&_msg, size);
+        }
+        
+        Frame(const std::string &string) {
+            
+            auto size = string.size();
+            void *data = new char(size);
+            memcpy(data, string.data(), size);
+            
+            zmq_msg_init_data(&_msg, data, size, &Frame::free_msg_data, NULL);
+        }
+
+        ~Frame() {
+            zmq_msg_close(&_msg);
+        }
+
+        size_t size() const {
+            return zmq_msg_size(&_msg);
+        }
+        
+        template <typename T>
+        T *data() const {
+            return (T *)zmq_msg_data(&_msg);
+        }
+        
+    private:
+        
+        static void free_msg_data(void *ptr, void *hint) {
+            delete (char *)ptr;
+        }
+        
+        mutable zmq_msg_t _msg;
         
     };
 
--- Messaging/Tests/FrameSpec.cpp
+++ Messaging/Tests/FrameSpec.cpp
@@ -9,12 +9,60 @@
 #include <ZingBDD/ZingBDD.h>
 
 #include "Frame.h"
+#include <string.h>
 
 namespace Messaging { namespace Specs {
    
     describe(Frame, {
     
-        Frame frame;
+        context("default constructor", {
+            
+            Frame frame;
+            
+            context("size", {
+
+                it("has size of zero", {
+                    
+                    expect(frame.size()).should.equal(0);
+                    
+                });
+                
+            });
+            
+        });
+        
+        context("size constructor", {
+           
+            size_t expectedSize = 64;
+            Frame frame(expectedSize);
+            
+            it("has expected size", {
+                
+                expect(frame.size()).should.equal(expectedSize);
+                
+            });
+            
+        });
+        
+        context("string constructor", {
+           
+            char expectedData[] = "this is a test";
+            
+            Frame frame(expectedData);
+            
+            it("has expected size", {
+                
+                expect(strlen(frame.data<char>())).should.equal(strlen(expectedData));
+                
+            });
+            
+            it("has expected data", {
+                
+                expect(strcmp(frame.data<char>(), expectedData)).should.equal(0);
+                
+            });
+            
+        });
         
     });
     

8a2750f7a3fab0634d2231eec9633cc9c8b88835
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-25 10:26:33 +0100

    [Messaging] Added Reactor class stub and spec

---
+++ Messaging/Classes/Reactor.h
@@ -0,0 +1,20 @@
+//
+//  Reactor.h
+//  Messaging
+//
+//  Created by Dave Meehan on 25/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#ifndef __Messaging__Reactor__
+#define __Messaging__Reactor__
+
+namespace Messaging {
+    
+    class Reactor {
+        
+    };
+    
+}
+
+#endif /* defined(__Messaging__Reactor__) */
---
+++ Messaging/Tests/ReactorSpec.cpp
@@ -0,0 +1,25 @@
+//
+//  ReactorSpec.cpp
+//  Messaging
+//
+//  Created by Dave Meehan on 25/04/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <ZingBDD/ZingBDD.h>
+
+#include "Reactor.h"
+
+namespace Messaging { namespace Specs {
+  
+    describe(Reactor, {
+        
+        it("exists", {
+            
+            Reactor r;
+            
+        });
+        
+    });
+    
+} }

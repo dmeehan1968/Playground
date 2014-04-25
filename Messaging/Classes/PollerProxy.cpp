a031f41c6bb4a0694b31bd93b573a414defeba2d
Dave Meehan <dave_meehan@replicated.co.uk>
2014-04-25 14:03:23 +0100

    [Messaging] Removed PollerProxy, no longer needed

--- Messaging/Classes/PollerProxy.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PollerProxy.cpp
-//  Messaging
-//
-//  Created by Dave Meehan on 22/04/2014.
-//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
-//
-
-#include "PollerProxy.h"

8eeac03ed513e731308691c951fd3714075df024
Dave Meehan <dave_meehan@replicated.co.uk>
2014-05-01 11:24:29 +0100

    [Messaging] Refactored StreamAdapter::EventFactory tests for readability

--- Messaging/Examples/StreamAdapterSpec.cpp
+++ Messaging/Examples/StreamAdapterSpec.cpp
@@ -34,61 +34,85 @@ namespace Messaging { namespace Examples {
                 
             });
 
-            it("returns malformed if initial frame is zero", {
+            context("first frame", {
                 
-                auto event = factory.parse(nullptr, 0, true);
+                std::shared_ptr<MalformedEvent> malformed;
                 
-                expect((bool)std::dynamic_pointer_cast<MalformedEvent>(event)).should.beTrue();
+                context("zero length", {
+                    
+                    beforeEach({
+                        
+                        auto event = factory.parse(nullptr, 0, true);
+                        
+                        malformed = std::dynamic_pointer_cast<MalformedEvent>(event);
+                        
+                    });
+
+                    
+                    it("returns malformed", {
+                        
+                        expect((bool)malformed).should.beTrue();
+                        
+                    });
+                    
+                });
+
+                context("no more", {
+
+                    beforeEach({
+                        
+                        auto event = factory.parse("ABC", 3, false);
+                        
+                        malformed = std::dynamic_pointer_cast<MalformedEvent>(event);
+                        
+                    });
+
+                    
+                    it("returns malformed", {
+                        
+                        expect((bool)malformed).should.beTrue();
+                        
+                    });
+                    
+                });
+
+                context("valid client id", {
+                    
+                    it("returns null", {
+                        
+                        auto event = factory.parse("ABCDE", 5, true);
+                        
+                        expect((bool)event).should.beFalse();
+                        
+                    });
+                    
+                });
+
                 
             });
 
-            it("returns malformed if initial frame has no more", {
+            context("second frame", {
                 
-                auto event = factory.parse("ABC", 3, false);
-
-                expect((bool)std::dynamic_pointer_cast<MalformedEvent>(event)).should.beTrue();
-                
-            });
-            
-            it("returns null with valid client id", {
-                
-                auto event = factory.parse("ABCDE", 5, true);
-                
-                expect((bool)event).should.beFalse();
-                
-            });
-
-            context("valid client id", {
-
-                StreamAdapter::StreamEvent::ClientID expectedClientId( { 'A', 'B', 'C', 'D', 'E' });
+                StreamAdapter::StreamEvent::ClientID expectedClientId({ 'A', 'B', 'C', 'D', 'E' });
                 
                 StreamAdapter::DataEvent::Data expectedData( { 'D', 'A', 'T', 'A' } );
                 
                 beforeEach({
                     
                     factory.parse(expectedClientId.data(), expectedClientId.size(), true);
-                    
                 });
-                
-                it("returns malformed if has more", {
-                    
-                    auto event = factory.parse(expectedData.data(), expectedData.size(), true);
-                    
-                    expect((bool)std::dynamic_pointer_cast<MalformedEvent>(event)).should.beTrue();
-                    
-                });
-                
-                context("data size is zero", {
 
+                context("no data", {
+                    
                     std::shared_ptr<StreamAdapter::ConnectionEvent> connectionEvent;
-
+                    
                     beforeEach({
-
+                        
                         auto event = factory.parse("", 0, false);
                         
                         connectionEvent = std::dynamic_pointer_cast<StreamAdapter::ConnectionEvent>(event);
                     });
-
+                    
                     it("returns ConnectionEvent", {
                         
                         expect((bool)connectionEvent).should.beTrue();
@@ -101,27 +125,46 @@ namespace Messaging { namespace Examples {
                         
                     });
 
-                    
                 });
 
-                context("data size is greater than zero", {
+                context("has more", {
+                    
+                    std::shared_ptr<MalformedEvent> malformed;
+                    
+                    beforeEach({
+                        
+                        auto event = factory.parse(expectedData.data(), expectedData.size(), true);
+                        
+                        malformed = std::dynamic_pointer_cast<MalformedEvent>(event);
+                        
+                    });
 
+                    it("returns malformed", {
+                        
+                        expect((bool)malformed).should.beTrue();
+                        
+                    });
+                    
+                });
+                
+                context("with data", {
+                    
                     std::shared_ptr<StreamAdapter::DataEvent> dataEvent;
                     
                     beforeEach({
-
+                        
                         auto event = factory.parse(expectedData.data(), expectedData.size(), false);
-
+                        
                         dataEvent = std::dynamic_pointer_cast<StreamAdapter::DataEvent>(event);
                         
                     });
-
+                    
                     it("returns DataEvent", {
-
+                        
                         expect((bool)dataEvent).should.beTrue();
                         
                     });
-
+                    
                     it("has expected client id", {
                         
                         expect(dataEvent->clientId()).should.equal(expectedClientId);
@@ -133,7 +176,7 @@ namespace Messaging { namespace Examples {
                         expect(dataEvent->data()).should.equal(expectedData);
                         
                     });
-
+                    
                 });
 
             });

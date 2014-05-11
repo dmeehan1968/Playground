f49c52ae1c3c91ba44f2b0c028b26a41a786dc36
Dave Meehan <dave_meehan@replicated.co.uk>
2014-05-11 20:47:32 +0100

    [Messaging] Added StreamServer stub and spec

---
+++ Messaging/Nodes/StreamServer.h
@@ -0,0 +1,21 @@
+//
+//  StreamServer.h
+//  Messaging
+//
+//  Created by Dave Meehan on 11/05/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#ifndef __Messaging__StreamServer__
+#define __Messaging__StreamServer__
+
+namespace Messaging { namespace Nodes { namespace StreamProtocol {
+    
+    class StreamServer {
+        
+    };
+    
+    
+} } }
+
+#endif /* defined(__Messaging__StreamServer__) */
---
+++ Messaging/Nodes/StreamServerSpec.cpp
@@ -0,0 +1,22 @@
+//
+//  StreamServerSpec.cpp
+//  Messaging
+//
+//  Created by Dave Meehan on 11/05/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <ZingBDD/ZingBDD.h>
+
+#include "StreamServer.h"
+
+namespace Messaging { namespace Nodes { namespace StreamProtocol {
+    
+    describe(StreamServer, {
+
+        StreamServer server;
+        
+        
+    });
+    
+} } }

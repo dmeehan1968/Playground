e842a852db44ea426359f8f495e9295ebc31b493
Dave Meehan <dave_meehan@replicated.co.uk>
2014-05-12 23:13:06 +0100

    [Messaging] Added SessionCodec::decode

--- Messaging/Nodes/SessionCodec.h
+++ Messaging/Nodes/SessionCodec.h
@@ -9,9 +9,249 @@
 #ifndef __Messaging__SessionCodec__
 #define __Messaging__SessionCodec__
 
+#include "Frame.h"
+
 namespace Messaging { namespace Nodes {
 
+    class AbstractMsg {
+    
+    public:
+        
+        AbstractMsg()
+        :
+            _state(State::Address)
+        {}
+        
+        virtual bool decode(const Frame &frame) {
+            
+            switch (_state) {
+                    
+                case State::Address:
+                    
+                    address = frame;
+                    _state = State::Delimiter;
+                    break;
+                    
+                case State::Delimiter:
+                    
+                    _state = State::Identity;
+                    break;
+                    
+                case State::Identity:
+                    
+                    identity = frame;
+                    _state = State::_Final;
+                    break;
+                    
+                case State::_Final:
+                    
+                    throw std::runtime_error("invalid frame for AbstractMsg");
+                    
+            }
+            
+            return isFinal();
+            
+        }
+        
+        virtual bool isFinal() const {
+            
+            return _state == State::_Final;
+            
+        }
+        
+        virtual AbstractMsg *clone() const {
+            return new AbstractMsg(*this);
+        };
+        
+        Frame address;
+        Frame identity;
+        
+    private:
+        
+        enum class State {
+            
+            Address,
+            Delimiter,
+            Identity,
+            _Final
+            
+        };
+        
+        State _state;
+        
+    };
+    
+    class AbcMsg : public AbstractMsg {
+        
+    public:
+        
+        AbcMsg(const AbstractMsg &msg)
+        :
+            AbstractMsg(msg)
+        {}
+        
+        virtual bool isFinal() const override {
+            return true;
+        }
+        
+        virtual AbcMsg *clone() const override {
+            return new AbcMsg(*this);
+        }
+        
+    };
+    
+    class DefMsg : public AbstractMsg {
+        
+    public:
+        
+        DefMsg(const AbstractMsg &msg)
+        :
+            AbstractMsg(msg),
+            _state(State::d)
+        {}
+        
+        virtual bool isFinal() const override {
+            return _state == State::_Final;
+        }
+        
+        virtual bool decode(const Frame &frame) override {
+        
+            switch (_state) {
+                    
+                case State::d:
+                {
+                    d = frame;
+                    _state = State::e;
+                    break;
+                }
+                    
+                case State::e:
+                {
+                    e = frame;
+                    _state = State::f;
+                    break;
+                }
+                    
+                case State::f:
+                {
+                    f = frame;
+                    _state = State::_Final;
+                    break;
+                }
+                    
+                default:
+                {
+                    throw std::runtime_error("invalid frame for DefMsg");
+                    break;
+                }
+            }
+            
+            return isFinal();
+        }
+        
+        Frame d;
+        Frame e;
+        Frame f;
+        
+    private:
+        
+        enum class State {
+            
+            d,
+            e,
+            f,
+            _Final
+            
+        };
+        
+        State _state;
+        
+    };
+    
     class SessionCodec {
+    
+    public:
+        
+        SessionCodec() {
+            
+            reset();
+            
+        }
+        
+        void reset() {
+            
+            _msgType = MsgType::AbstractMsg;
+            _msg = std::make_shared<AbstractMsg>();
+            
+        }
+        
+        std::shared_ptr<AbstractMsg> decode(const Frame &frame) {
+        
+            switch (_msgType) {
+                    
+                case MsgType::AbstractMsg:
+                {
+                    if (_msg->decode(frame)) {
+                        
+                        // detect message types and transform
+                        
+                        if (_msg->identity == "ABC") {
+                            
+                            _msg = std::make_shared<AbcMsg>(*_msg);
+                            _msgType = MsgType::_Final;
+                            
+                        } else if (_msg->identity == "DEF") {
+
+                            _msg = std::make_shared<DefMsg>(*_msg);
+                            _msgType = MsgType::DefMsg;
+
+                        } else {
+                         
+                            throw std::runtime_error("unexpected identity");
+                            
+                        }
+                        
+                    }
+                    break;
+                }
+                    
+                case MsgType::DefMsg:
+                // Repeat for other message types
+                {
+                    // specific message decode handles error
+
+                    _msg->decode(frame);
+                    break;
+                }
+                    
+                default:
+                {
+                    throw std::runtime_error("invalid message type");
+                    break;
+                }
+            
+            }
+            
+            return _msg->isFinal() ? _msg : nullptr;
+            
+        }
+        
+    private:
+        
+        enum class MsgType {
+            
+            AbstractMsg,
+            
+            // insert message types here
+            AbcMsg,
+            DefMsg,
+            
+            _Final
+            
+        };
+        
+        MsgType _msgType;
+        std::shared_ptr<AbstractMsg> _msg;
         
     };
     
--- Messaging/Nodes/SessionCodecSpec.cpp
+++ Messaging/Nodes/SessionCodecSpec.cpp
@@ -18,10 +18,149 @@ namespace Messaging { namespace Nodes { namespace Specs {
             
             SessionCodec codec;
             
+            codec.decode(Frame());
+            
         });
 
+        context("decoding", {
+            
+            context("abc message", {
+                
+                SessionCodec codec;
+                Frame expectedAddress("12345");
+                Frame expectedIdentity("ABC");
+                
+                it("accepts address", {
+                    
+                    expect((bool)codec.decode(expectedAddress)).should.beFalse();
+                    
+                });
+                
+                it("accepts delimiter", {
+                    
+                    expect((bool)codec.decode(Frame())).should.beFalse();
+                    
+                });
+                
+                context("creates on identity", {
+                    
+                    auto msg = codec.decode(expectedIdentity);
+                    
+                    it("creates", {
+                        
+                        expect((bool)msg).should.beTrue();
+                        
+                    });
+
+                    it("has expected address", {
+                        
+                        expect(msg->address).should.equal(expectedAddress);
+                        
+                    });
+                    
+                    it("has expected identity", {
+                        
+                        expect(msg->identity).should.equal(expectedIdentity);
+                        
+                    });
+                    
+                    it("is expected type", {
+                        
+                        expect((bool)std::dynamic_pointer_cast<AbcMsg>(msg)).should.beTrue();
+                        
+                    });
+
+                });
+                
+            });
+            
+            context("def message", {
+                
+                SessionCodec codec;
+                Frame address("12345");
+                Frame identity("DEF");
+                
+                it("accepts address", {
+                    
+                    expect((bool)codec.decode(address)).should.beFalse();
+                });
+
+                it("accepts delimiter", {
+                    
+                    expect((bool)codec.decode(Frame())).should.beFalse();
+                    
+                });
+                
+                it("accepts identity", {
+                    
+                    expect((bool)codec.decode(identity)).should.beFalse();
+                    
+                });
+
+                it("accepts d frame", {
+                    
+                    expect((bool)codec.decode(Frame("d"))).should.beFalse();
+                    
+                });
+                
+                it("accepts e frame", {
+                    
+                    expect((bool)codec.decode(Frame("e"))).should.beFalse();
+                    
+                });
+                
+                context("accepts f frame", {
+                    
+                    auto msg = codec.decode(Frame("f"));
+                    
+                    it("creates msg", {
+                        
+                        expect((bool)msg).should.beTrue();
+                        
+                    });
+                    
+                    it("has expected address", {
+                        
+                        expect(msg->address).should.equal(address);
+                        
+                    });
+                    
+                    it("has expected identity", {
+                        
+                        expect(msg->identity).should.equal(identity);
+                        
+                    });
+                    
+                    context("msg specific", {
+                        
+                        auto def = std::dynamic_pointer_cast<DefMsg>(msg);
+                        
+                        it("has expected d value", {
+                            
+                            expect(def->d).should.equal(Frame("d"));
+                            
+                        });
+                        
+                        it("has expected e value", {
+                            
+                            expect(def->e).should.equal(Frame("e"));
+                            
+                        });
+                        
+                        it("has expected f value", {
+                            
+                            expect(def->f).should.equal(Frame("f"));
+                            
+                        });
+                        
+                    });
+                    
+                });
+                
+            });
+
+        });
         
     });
-    
     
 } } }
